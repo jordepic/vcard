9e2b22a55b0de824920f7c0858eb32dbc364921b
Jordan Epstein <jordepstein@gmail.com>
2019-07-07 12:22:08 -0400

    Creating New Viewcontrollers

---
+++ vcard/ContactsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ContactsViewController.swift
+//  vcard
+//
+//  Created by Jordan Epstein on 7/7/19.
+//  Copyright © 2019 Jordan Epstein. All rights reserved.
+//
+
+import UIKit
+
+class ContactsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
